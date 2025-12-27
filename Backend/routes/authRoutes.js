e6528db57d7be82c8783deeea2c7d497f4b0206f
Bantipatel20 <patelbanti993@gmail.com>
2025-12-27 10:43:11 +0530

    Create authRoutes.js

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import auth from '../middleware/auth.js';
+import { register, login } from '../controllers/User.js';
+
+const router = express.Router();
+
+
+router.post('/register', register);
+router.post('/login', login);
+
+
+router.get('/profile', auth, (req, res) => {
+  res.json({ msg: 'Protected route accessed', userId: req.userId });
+});
+
+export default router;
